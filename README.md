a089247081b8d9f68e0cfcd28abb822e09af3ffd
acnash <distributed.science@googlemail.com>
2025-06-06 11:03:47 +0100

    Initial deposit.

--- README.md
+++ README.md
@@ -5,6 +5,9 @@ Scripts for analysing Molecular Dynamics trajectories and molecular models.
 ## Spherical residue extraction
 All residues within a specified distance (in angstroms) from a specified residue are extracted from an input PDB file and saved to an output residue file. 
 
+### Execution
+`python extract_residues_around_CA.py`
+
 ### Options
 - `input_file` - Path and file name of a clean PDB input file.
 - `output_file` - Path and file name of where to save residues.
@@ -14,8 +17,21 @@ All residues within a specified distance (in angstroms) from a specified residue
 ## Extract SASA residues
 Uses FreeSASA to isolate the solvent accessible surface areas of a protein (from a PDB file) and saves those residues to a PDB file. This will not discriminate chains. Every molecule in the input file is parsed.
 
+Threshold for what we consider as a "surface" residue:
+- < 5 Å²	Very buried (ignore)
+- 5–30 Å²	Partially exposed residues
+- 30–50+ Å²	Fully exposed residues
+
+A residue is considered a _surface residue_ if its surface areas is greater than the surface threshold.
+
+### External dependencies 
 A dependency on having FreeSASA installed. This works on my Mac laptop, but not on my Windows machine.
+
+### Execution
+`python surface_residues.py
 
 ### Options
 - `input_file` - Path and file name of a clean PDB input file.
 - `output_file` - Path and file name of where to save residues.
+- `surface_threshold` - Threshold in Å² that defines when a residue becomes a surface residue. Set to 30.  
+- `residue_file` - Output text file location (include file path) of stored residue IDs that make up the surface.
--- src/surface_residues.py
+++ src/surface_residues.py
@@ -1,48 +1,68 @@
+import argparse
+
 import MDAnalysis as mda
 import freesasa
 
-# Load the structure
-u = mda.Universe("../trajectories/cluster_40360_free.pdbqt")
+def main():
+    parser = argparse.ArgumentParser(description="Silent Wealth")
 
-# Select the protein atoms
-protein = u.select_atoms("protein")
-protein.write("temp_protein.pdb")
+    parser.add_argument("--input", type=str, required=True,
+                        help="Input PDB file (include file path). Make sure it's clean..")
+    parser.add_argument("--output", type=str, required=True, help="Output PDB file location (include file path).")
+    parser.add_argument("--surface_threshold", type=int, required=True, help="Threshold in Å² that defines when a residue becomes a surface residue. Set to 30.")
+    parser.add_argument("--residue_file", type=str, required=True, help="Output text file location (include file path) of stored residue IDs that make up the surface.")
 
-# Use freesasa to load and calculate SASA
-structure = freesasa.Structure("temp_protein.pdb")
-result = freesasa.calc(structure)
+    args = parser.parse_args()
+    pdb_file = args.input
+    output_file = args.output
+    residue_file = args.residue_file
+    surface_threshold = args.surface_threshold
 
-# Get per-residue SASA
-residue_areas = result.residueAreas()
+    # Load the structure
+    u = mda.Universe(pdb_file)
 
-# Threshold for what we consider "surface"
-# < 5 Å²	Very buried (ignore)
-# 5–30 Å²	Partially exposed residues
-# >30–50+ Å²	Fully exposed residues
-surface_threshold = 30.0  # Å²
+    # Select the protein atoms
+    protein = u.select_atoms("protein")
+    protein.write("temp_protein.pdb")
 
-# Collect surface residues
-surface_residues = []
-for chain in residue_areas:
-    for resnum, resinfo in residue_areas[chain].items():
-        total_area = resinfo.total  # attribute
-        if total_area > surface_threshold:
-            surface_residues.append((chain, resinfo.residueType, resnum))
+    # Use freesasa to load and calculate SASA
+    structure = freesasa.Structure("temp_protein.pdb")
+    result = freesasa.calc(structure)
 
-selection = []
-for chain, restype, resnum in surface_residues:
-    if chain == "X":
-        selection.append(f"resid {resnum}")
-    else:
-        selection.append(f"segid {chain} and resid {resnum}")
+    # Get per-residue SASA
+    residue_areas = result.residueAreas()
 
-surface_sel = u.select_atoms(" or ".join(selection))
+    # Threshold for what we consider "surface"
+    # < 5 Å²	Very buried (ignore)
+    # 5–30 Å²	Partially exposed residues
+    # >30–50+ Å²	Fully exposed residues
+    #surface_threshold = 30.0  # Å²
 
-surface_sel.write("surface_residues.pdb")
+    # Collect surface residues
+    surface_residues = []
+    for chain in residue_areas:
+        for resnum, resinfo in residue_areas[chain].items():
+            total_area = resinfo.total  # attribute
+            if total_area > surface_threshold:
+                surface_residues.append((chain, resinfo.residueType, resnum))
 
-# Print the surface residues
-with open("MMP1_free_residues.txt", "w") as file:
-    for chain, resname, resnum in surface_residues:
-        ca_atom = u.select_atoms(f"resid {resnum} and name CA")
-        ca_pos = ca_atom.positions[0]
-        file.write(f"{resname} {resnum} {chain} {ca_pos[0]:.3f} {ca_pos[1]:.3f} {ca_pos[2]:.3f}\n")
+    selection = []
+    for chain, restype, resnum in surface_residues:
+        if chain == "X":
+            selection.append(f"resid {resnum}")
+        else:
+            selection.append(f"segid {chain} and resid {resnum}")
+
+    surface_sel = u.select_atoms(" or ".join(selection))
+
+    surface_sel.write(output_file)
+
+    # Print the surface residues
+    with open(residue_file, "w") as file:
+        for chain, resname, resnum in surface_residues:
+            ca_atom = u.select_atoms(f"resid {resnum} and name CA")
+            ca_pos = ca_atom.positions[0]
+            file.write(f"{resname} {resnum} {chain} {ca_pos[0]:.3f} {ca_pos[1]:.3f} {ca_pos[2]:.3f}\n")
+
+if __name__ == "__main__":
+    main()
